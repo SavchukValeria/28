8ec51c838ac46853f85a539c67701cf061bb82cd
SavchukValeria <V.Savchuk_FIT_12_21_B_d@knute.edu.ua>
2022-10-04 12:50:42 +0300

    28
    28

---
+++ ClassLibrary5/ClassLibrary1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ClassLibrary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Address solution\n");
+            Address someAddress = new Address();
+
+            someAddress.Index = 02138;
+            someAddress.Country = "Ukraine";
+            someAddress.City = "Kyiv";
+            someAddress.Street = "Unnamed";
+            someAddress.House = 8;
+            someAddress.Apartment = 310;
+
+            Console.WriteLine(someAddress.GetAddress());
+
+            Console.ReadKey();
+        }
+    }
+}
